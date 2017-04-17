edfc22920cba80518ae94df8f47f7c5a97932726
Chen Meng <observerchan@gmail.com>
2017-04-17 18:06:07 +0800

    .

--- wsi/reg_program.py
+++ wsi/reg_program.py
@@ -14,28 +14,9 @@ class RegApp:
 
 def get_reg_apps():
     regapps = []
-
-    for i in (r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-              r'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'):
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, i, 0, winreg.KEY_ALL_ACCESS)
-        for j in range(winreg.QueryInfoKey(key)[0]):
-            try:
-                each_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                          i + '\\' + winreg.EnumKey(key, j),
-                                          0,
-                                          winreg.KEY_ALL_ACCESS
-                                          )
-
-                p = RegApp(display_name=winreg.QueryValueEx(each_key, 'DisplayName')[0],
-                           display_version=winreg.QueryValueEx(each_key, 'DisplayVersion')[0],
-                           publisher=winreg.QueryValueEx(each_key, 'Publisher')[0],
-                           uninstall_string=winreg.QueryValueEx(each_key, '')
-                           )
-                regapps.append(p)
-
-            except WindowsError:
-                pass
-
+    for key in (r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+                r'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'):
+        regapps.extend(winger.Tips(key).items())
     return regapps
 
 
